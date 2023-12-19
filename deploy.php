8c52fbc5445169058570cbe6b7bc8edf40c6444b
Pascale Beier <mail@pascalebeier.de>
2023-12-19 13:32:55 +0100

    Update to PHP 8.2

--- deploy.php
+++ deploy.php
@@ -8,24 +8,40 @@ new \SourceBroker\DeployerExtendedTypo3\Loader();
 set('repository', 'https://github.com/Haus-Marienmunster/Site.git');
 set('web_path', 'public/');
 set('composer_channel', 2);
+set('bin/composer', '/composer.phar');
 set('shared_files', ['.env', get('web_path') . 'typo3conf/AdditionalConfiguration.php']);
 
-host('live')
-    ->hostname('ferien-in-holte.de')
-    ->set('branch', 'main')
-    ->set('public_urls', ['https:/ferien-in-holte.de/'])
-    ->set('deploy_path', '/var/www/vhosts/ferien-in-holte.de/site')
-    ->set('ssh_type', 'native')
+host('local')
+    ->setHostname('local')
+    ->set('deploy_path', getcwd())
+    ->set('public_urls', ['https://ferien-in-holte.ddev.site'])
     ->set('db_databases', [
         'database_default' => [
             (new \SourceBroker\DeployerExtendedDatabase\Driver\EnvDriver())->getDatabaseConfig()
         ]
     ]);
 
-host('local')
-    ->hostname('local')
-    ->set('deploy_path', getcwd())
-    ->set('public_urls', ['https://ferien-in-holte.ddev.site'])
+
+//host('live')
+//    ->setHostname('ferien-in-holte.de')
+//    ->set('branch', 'main')
+//    ->setRemoteUser('ferien-in-holte.de_h5gwmhj7o3f')
+//    ->set('public_urls', ['https://ferien-in-holte.de/'])
+//    ->set('deploy_path', '/var/www/vhosts/ferien-in-holte.de/site/')
+//    ->set('db_databases', [
+//        'database_default' => [
+//            (new \SourceBroker\DeployerExtendedDatabase\Driver\EnvDriver())->getDatabaseConfig()
+//        ]
+//    ]);
+
+host('live')
+    ->setHostname('202.61.233.51')
+    ->set('branch', 'main')
+    ->setRemoteUser('hosting194366')
+    ->set('http_user', 'hosting194366')
+    ->set('writable_mode', 'chmod')
+    ->set('public_urls', ['https://ferien-in-holte.de/'])
+    ->set('deploy_path', '/ferien-in-holte.de')
     ->set('db_databases', [
         'database_default' => [
             (new \SourceBroker\DeployerExtendedDatabase\Driver\EnvDriver())->getDatabaseConfig()
